3ee71ee4c4fe39fe525b4c395a62e20f22715515
deishelon <deishelon@gmail.com>
2019-01-20 18:29:51 +1300

    Initial commit with working example + lib

--- app/src/main/java/com/deishelon/roundedbottomsheetexample/MyRoundedBottomSheet.kt
+++ app/src/main/java/com/deishelon/roundedbottomsheetexample/MyRoundedBottomSheet.kt
@@ -11,9 +11,4 @@ class MyRoundedBottomSheet: RoundedBottomSheetDialogFragment(){
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.dialog_2_my_rounded_bottom_sheet, container, false)
     }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-    }
 }
